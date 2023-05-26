8545dcf6d015db7da92978ed15c46040a53ee8c7
Dio Tendean <tendeanclaudio1234@gmail.com>
2023-05-26 19:08:23 +0800

    Initial Commit

---
+++ AppChalangeSlicing01/AppChalangeSlicing01App.swift
@@ -0,0 +1,17 @@
+//
+//  AppChalangeSlicing01App.swift
+//  AppChalangeSlicing01
+//
+//  Created by Claudio Tendean on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AppChalangeSlicing01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
